52f64ceb09cb283b32bae6f93199d5b8971fbe8e
PoppyG <kashwathaman@gmail.com>
2026-05-28 10:28:19 +0530

    add adnroid app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chennaiturboriders.racing',
+  appName: 'chennai-turbo-riders',
+  webDir: 'out'
+};
+
+export default config;
